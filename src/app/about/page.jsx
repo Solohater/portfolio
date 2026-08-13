920e8c1b4d736afb7a4c1bd7c7cc1e51cf6bab91
Yosef Ayalew <yosefayalew56@gmail.com>
2026-08-13 12:15:03 +0300

    feat: use photojo.png profile photo

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -31,7 +31,7 @@ const AboutPage = () => {
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY IMAGE */}
               <Image
-              src="/photojo.jpg"
+              src="/photojo.png"
               alt=""
               width={112}
               height={112}
